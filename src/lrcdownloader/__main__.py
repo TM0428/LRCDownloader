970a0a8d6cac95e8180172239b211fbb17542e8c
t.m <takashi.orion.999.428@gmail.com>
2024-06-30 14:32:30 +0900

    add rlclib

--- src/lrcdownloader/__main__.py
+++ src/lrcdownloader/__main__.py
@@ -1,2 +1,3 @@
 if __name__ == "__main__":
-    print("Hello, World!")
+    from lrcdownloader.lrclib import main
+    main()
---
+++ src/lrcdownloader/lrclib.py
@@ -0,0 +1,14 @@
+import requests
+
+BASE_URL = "https://lrclib.net"
+
+def search_lyrics(q):
+    url = f"{BASE_URL}/api/search?q={q}"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+def main():
+    lrcs = search_lyrics("Only My Railgun")
+    for lrc in lrcs:
+        print(lrc["artistName"], lrc["trackName"])
